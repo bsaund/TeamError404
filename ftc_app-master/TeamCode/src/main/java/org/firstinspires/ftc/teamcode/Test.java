a055b6426d8e1df613e2a8b13b6c11ee70ad391d
benkorch <korchben@gmail.com>
2016-11-30 16:05:23 -0800

    Added "wait for start"
    This change was made at competition, also indented code for alignment.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -5,30 +5,39 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name= "Test teleop", group = "test")
 public class Test extends LinearOpMode {
-    DcMotor l;
-    DcMotor r;
-    DcMotor conveyor;
-    DcMotor shooter;
+    DcMotor l;//left drive
+    DcMotor r;//right drive
+    DcMotor conveyor;//conveyor
+    DcMotor shooter;//shooter
+    DcMotor la;//left arm
     double lc; //left control
     double rc; //right control
     double cvbc; //conveyor control
     double sc; // shooter control
-    public void runOpMode() throws InterruptedException{
-        l = hardwareMap.dcMotor.get("l");
-        r = hardwareMap.dcMotor.get("r");
-        conveyor = hardwareMap.dcMotor.get("cvb");
-        shooter = hardwareMap.dcMotor.get("s");
-        telemetry.addData("Say", "YOU ARE A NERD!!!!!");
-        telemetry.update();
-        while (opModeIsActive()) {
-            lc = gamepad1.left_stick_y;
-            rc = -gamepad1.right_stick_y;
-            cvbc = gamepad2.left_stick_y;
-            sc = gamepad2.left_stick_y;
-            l.setPower(lc);
-            r.setPower(rc);
-            conveyor.setPower(-cvbc);
-            shooter.setPower(-sc);
+    double ac;//arm control
+
+    public void runOpMode() throws InterruptedException {
+        l = hardwareMap.dcMotor.get("l");//setup LOL
+        r = hardwareMap.dcMotor.get("r");// Same LOL
+        conveyor = hardwareMap.dcMotor.get("cvb");//LOLOLOLOLOLOLOLOLOLOLOLOLOL
+        shooter = hardwareMap.dcMotor.get("s");//TROLOLOLOLALOLLALALLOL
+        la = hardwareMap.dcMotor.get("la");//such funny much wow
+        telemetry.addData("", "YOU ARE A NERD!!!!!");//LOL--FYI- this does nothing
+        telemetry.update();//same here
+        waitForStart();
+        {
+            while (opModeIsActive()) {
+                lc = gamepad1.left_stick_y;
+                rc = -gamepad1.right_stick_y;
+                cvbc = gamepad2.left_stick_y;
+                sc = gamepad2.right_stick_y;
+                ac = gamepad2.right_trigger - gamepad2.left_trigger;
+                la.setPower(ac);
+                l.setPower(lc);
+                r.setPower(rc);
+                conveyor.setPower(cvbc);
+                shooter.setPower(-sc);
+            }
         }
     }
 }
