2bff62718c515c76ce8b83d905801824e05e7f14
lillianellis <lilymae176@gmail.com>
2016-12-04 13:41:52 -0800

    Auto Test
    We tried to make robot go forward and added gyro

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
@@ -15,14 +15,32 @@ public class Autotest extends LinearOpMode {
     DcMotor r;
     DcMotor conveyor;
     DcMotor shooter;
+    DcMotor la;//left arm
     SensorAdafruitIMU gyro;
     double lc; //left control
     double rc; //right control
     double cvbc; //conveyor control
     double sc; // shooter control
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
+        l = hardwareMap.dcMotor.get("l");//setup LOL
+        r = hardwareMap.dcMotor.get("r");// Same LOL
+        conveyor = hardwareMap.dcMotor.get("cvb");//LOLOLOLOLOLOLOLOLOLOLOLOLOL
+        shooter = hardwareMap.dcMotor.get("s");//TROLOLOLOLALOLLALALLOL
+        la = hardwareMap.dcMotor.get("la");//such funny much wow
+        telemetry.addData("", "YOU ARE A NERD!!!!!");//LOL--FYI- this does nothing
+        telemetry.update();//same here1
+        waitForStart();
+        cvbc=1;
         while(opModeIsActive()){
+            l.setPower(cvbc);
+            cvbc=-cvbc;
+            sleep(1500);
+            l.setPower(0);
+            sleep(1500);
+
+
 
         }
+
     }
 }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-@TeleOp(name= "Test teleop", group = "test")
+@TeleOp(name= "Lillian testing", group = "test")
 public class Test extends LinearOpMode {
     DcMotor l;//left drive
     DcMotor r;//right drive
@@ -41,3 +41,5 @@ public class Test extends LinearOpMode {
         }
     }
 }
+
+
