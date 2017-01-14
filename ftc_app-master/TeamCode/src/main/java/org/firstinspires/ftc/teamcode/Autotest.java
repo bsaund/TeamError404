d8a9254e63d87260cd3f6474df8b9562f879b614
tazule <jxfiorito@comcast.net>
2017-01-14 15:47:01 -0800

    smoop

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
@@ -9,54 +9,63 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  */
 @Autonomous(name="Auto test", group="test")
 public class Autotest extends LinearOpMode {
-    public void DriveFwdIn(double In){
+    public void DriveFwdIn(double In) {
         double a;
         double startangle;
         double rotations;
         double tics;
         startangle = gyro.getHeading();
         a = r.getCurrentPosition();
-        rotations = In*57.295779513082320876798154814105;
-        if(In>0){
-            while(r.getCurrentPosition() - a < rotations){
+        rotations = In * 57.295779513082320876798154814105;
+        if (In > 0) {
+            while (r.getCurrentPosition() - a < rotations) {
                 tics = r.getCurrentPosition() - a;
-                r.setPower((1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.8*Math.pow(((tics-rotations)/rotations),2))));
-                l.setPower(-(1/(1+.1*Math.pow(2,startangle-gyro.getHeading())))*(1-(.8*Math.pow(((tics-rotations)/rotations),2))));
-                telemetry.addData("right",(1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.9*abs((tics-rotations)/rotations))));
-                telemetry.addData("left",(1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.9*abs((tics-rotations)/rotations))));
+                r.setPower((1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.8 * Math.pow(((tics - rotations) / rotations), 2))));
+                l.setPower(-(1 / (1 + .1 * Math.pow(2, startangle - gyro.getHeading()))) * (1 - (.8 * Math.pow(((tics - rotations) / rotations), 2))));
+                telemetry.addData("right", (1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.9 * abs((tics - rotations) / rotations))));
+                telemetry.addData("left", (1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.9 * abs((tics - rotations) / rotations))));
                 telemetry.update();
             }
-        }else{
-            while(r.getCurrentPosition() - a > rotations){
+        } else {
+            while (r.getCurrentPosition() - a > rotations) {
                 tics = r.getCurrentPosition() - a;
-                r.setPower(-(1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.8*Math.pow(((tics-rotations)/rotations),2))));
-                l.setPower((1/(1+.1*Math.pow(2,startangle-gyro.getHeading())))*(1-(.8*Math.pow(((tics-rotations)/rotations),2))));
-                telemetry.addData("right",-(1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.9*abs((tics-rotations)/rotations))));
-                telemetry.addData("left",-(1/(1+.1*Math.pow(2,gyro.getHeading()-startangle)))*(1-(.9*abs((tics-rotations)/rotations))));
+                r.setPower(-(1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.8 * Math.pow(((tics - rotations) / rotations), 2))));
+                l.setPower((1 / (1 + .1 * Math.pow(2, startangle - gyro.getHeading()))) * (1 - (.8 * Math.pow(((tics - rotations) / rotations), 2))));
+                telemetry.addData("right", -(1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.9 * abs((tics - rotations) / rotations))));
+                telemetry.addData("left", -(1 / (1 + .1 * Math.pow(2, gyro.getHeading() - startangle))) * (1 - (.9 * abs((tics - rotations) / rotations))));
                 telemetry.update();
             }
         }
     }
-    public void TurnNDegrees(double degrees){
+
+    /**
+     * Robot turns the degrees given
+     * @param degrees
+     */
+    public void TurnNDegrees(double degrees) {
         double startangle;
         double theta;
-        startangle = gyro.getHeading();
-        while(abs(gyro.getHeading()-degrees)<.1) {
-            if (degrees > 0) {
-                while (gyro.getHeading() < degrees) {
-                    theta = gyro.getHeading()-startangle;
-                    r.setPower((1 - (.8 * Math.pow(((theta - degrees) / degrees), 2))));
-                    l.setPower((1 - (.8 * Math.pow(((theta - degrees) / degrees), 2))));
-                }
-            } else {
-                while (gyro.getHeading() > degrees) {
-                    theta = gyro.getHeading()-startangle;
-                    r.setPower(-(1 - (.8 * Math.pow(((theta - degrees) / degrees), 2))));
-                    l.setPower(-(1 - (.8 * Math.pow(((theta - degrees) / degrees), 2))));
-                }
+        startangle = gyro.getIntegratedZValue();
+        double goal_angle = startangle + degrees;
+        while (abs(gyro.getIntegratedZValue() - goal_angle) > 5) {
+            //theta = gyro.getHeading()-startangle;
+            //double turn_rate = (-(1 - (.8 * Math.pow(((theta - degrees) / degrees), 2))));
+            double turn_rate = .5;
+
+            if (gyro.getIntegratedZValue() > goal_angle){
+                    turn_rate = -turn_rate;
             }
+            r.setPower(turn_rate);
+            l.setPower(turn_rate);// not negative b/c left motor reversed
+            telemetry.addData("turn rate:", turn_rate);
+            telemetry.addData("Gyro heading:", gyro.getIntegratedZValue());
+            telemetry.addData("goal_angle", goal_angle);
+            telemetry.update();
         }
+        r.setPower(0);
+        l.setPower(0);
     }
+
     DcMotor l;
     DcMotor r;
     ModernRoboticsI2cGyro gyro;
@@ -86,13 +95,13 @@ public class Autotest extends LinearOpMode {
         waitForStart();
         telemetry.addData(">", "pink fluffy unicorns dancing on rainbows");
         telemetry.update();
-        DriveFwdIn(-6.25);
+        //DriveFwdIn(-6.25);
         telemetry.addData(">", "pink fluffy unicorns dancing on rainbows");
         telemetry.update();
         TurnNDegrees(-45);
         telemetry.addData(">", "pink fluffy unicorns dancing on rainbows");
         telemetry.update();
-        DriveFwdIn(-60);
+        //DriveFwdIn(-60);
         telemetry.addData(">", "Let's test your knowledge and see what you've learned so far! What colour are the unicorns?");
         telemetry.update();
         sleep(2000);
@@ -100,4 +109,3 @@ public class Autotest extends LinearOpMode {
         telemetry.update();
     }
 }
-
